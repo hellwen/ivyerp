3d32bfd9d2a59467bb1fd32fce662b1a15ce4f06
hellwen <hellwen.wu@gmail.com>
2013-09-07 08:09:54 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ivyerp::Application.config.secret_key_base = 'a9440cfd55b0f3a5e09ccb9824832f14771dac50751909bb08190adcaf6ead99a0f9a2a3a180cc7c73079199181ad548fc3835b44252a855be0a53ffe0d24b51'
