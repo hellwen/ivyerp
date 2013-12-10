a526527538ae5c0fecceba85a615f304015adee2
hellwen <hellwen.wu@gmail.com>
2013-12-10 23:58:36 +0800

    init

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,5 @@
 class ProductsController < AuthorizedController
-  # Actions
-  def index
-    @products = apply_scopes(Product).paginate(:page => params[:page], :per_page => params[:per_page])
-    #@products = apply_scopes(Product).accessible_by(current_ability, :list).includes(:name).paginate(:page => params[:page], :per_page => params[:per_page])                                                                      
-    index!
-  end
+  # Scopes
+  has_scope :by_text
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,12 +2,21 @@ class Product < ActiveRecord::Base
   attr_accessible :code, :customer_id, :name, :remark, :specification
   validates_presence_of :code, :name, :customer_id
 
+  #scope :by_name, -> name { where("name like :name", :name => '%' + name + '%') }
+  #User.where(users[:name].matches("%#{user_name}%"))
+
   def attr_list
     [:code, :name, :customer, :specification]
   end
 
   belongs_to :customer
   has_many :stock_products, :dependent => :nullify
+
+  #scope :by_name, joins(:stock_products),  -> name { where("products.name like :name", :name => "%" + name + "%") }
+
+  def self.by_text(text)
+    joins(:customer).where("products.name like ? or customers.name like ?", "%" + text + "%", "%" + text + "%")
+  end
 
   def to_s
     name.present? ? name : ''
